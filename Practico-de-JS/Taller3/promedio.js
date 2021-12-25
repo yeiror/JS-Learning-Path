51501cb2281627f5888819312878416929ac4ab1
Yair Riascos <yeiror@gmail.com>
2021-12-25 18:48:05 -0500

    Version 2.0

--- Practico-de-JS/Taller3/promedio.js
+++ Practico-de-JS/Taller3/promedio.js
@@ -2,36 +2,50 @@ let lista =[];
 let listado = document.getElementById("listado");
 let dato = document.getElementById("numeroLista");
 let resultado = document.getElementById("resultado");
+let elementos = document.getElementById("contenedorLista");
+
 
 function agregarLista(){
-    
-    lista.push(parseInt(dato.value));
-    listado.innerText = `Tu lista lleva: ${lista.length} elementos si es correcto, presiona el botón 'Calcular promedio'`;
-}
+     
+    if(dato.value != ""){
+        elementos.innerText="Los números en la lista son:" ;
+        lista.push(parseInt(dato.value));
+        listado.innerText = `Tu lista lleva: ${lista.length} elementos si es correcto, presiona el botón 'Calcular promedio'`;
+        
+        lista.forEach(elemento=>{
+            elementos.innerText+= "\n"+elemento  ;
+        });
 
+        elementos.style.display="inline";
+    }
+    else{
+       console.log("introduce un número válido");
+    }
+    calcularPromedio();
+  
+       
+}
 function limpiar(){
    
     lista = [];
     listado.innerText = `La lista está vacía`;
+    resultado.innerText = `No hay ningun elemento en la lista`;
+    elementos.style.display="none";
 }
 
 function calcularPromedio(){
-    //let sumaLista = 0
-    //for(let i=0;i< lista.length;i++){
-    //    sumaLista+= lista[i];
-    //}
-
-    //También podemos utilizar el método reduce
     if(lista.length>=1){
-    const sumaLista = lista.reduce(
-        function (valorAcumulado, nuevoElemento){
-            return valorAcumulado + nuevoElemento;
+        const sumaLista = lista.reduce(
+            function (valorAcumulado, nuevoElemento){
+                return valorAcumulado + nuevoElemento;
+            }
+        );  
+        promedio = sumaLista/lista.length
+        resultado.innerText = `El promedio de la lista es: ${promedio}`;
         }
-    );  
-    promedio = sumaLista/lista.length
-    resultado.innerText = `El promedio de la lista es: ${promedio}`;
-    }
-    else{
-        resultado.innerText = `No hay ningun elemento en la lista`;
-    }
+        else{
+            resultado.innerText = `No hay ningun elemento en la lista`;
+        }
 }
+
+
